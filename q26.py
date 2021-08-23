c0415453d2dd2bc130aba369bd03b68be8aba56d
Harry <59126651+hdowens@users.noreply.github.com>
2021-08-23 12:21:53 +0100

    Update q26.py

--- q26.py
+++ q26.py
@@ -1,1 +1,18 @@
+def multOrd(n):
+    tmp = 1
+    while 10 ** tmp % n != 1:
+        tmp += 1
+    return tmp
 
+localmax = 0
+localind = 0
+for i in range(2,1000):
+    if i % 2 == 0 or i % 5 == 0:
+        continue
+    else:
+        length = multOrd(i)
+        if length > localmax:
+            localmax = length
+            localind = i
+
+print("Ans is -> ",localind)
