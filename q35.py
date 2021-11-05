448acc6ba85e791d2b273752a6716324a2c97525
Harry <59126651+hdowens@users.noreply.github.com>
2021-11-05 09:52:03 +0000

    Update q35.py

--- q35.py
+++ q35.py
@@ -1,15 +1,10 @@
 import math
 
 def is_prime(n):
-    if n == 2:
-        return True
-    if n % 2 == 0 or n <= 1:
+    if (n % 2 == 0 and n > 2) or n <= 1:
         return False
-
-    sqr = int(math.sqrt(n)) + 1
-
-    for divisor in range(3, sqr, 2):
-        if n % divisor == 0:
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
             return False
     return True
 
