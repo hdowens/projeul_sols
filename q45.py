d49538fec9f54fbb2d89d23cb7c05369177030da
Harry <59126651+hdowens@users.noreply.github.com>
2022-05-10 16:37:03 +0100

    Create q45.py

---
+++ q45.py
@@ -0,0 +1,22 @@
+from timeit import default_timer
+from math import sqrt
+
+def isPentagonal(n):
+    n = (sqrt(1 + 24*n)+1)/6
+    return n.is_integer()
+
+def get_answer():
+    for i in range(144,99999):
+        n = i * (2 * i - 1)
+        if isPentagonal(n):
+            print(n)
+
+if __name__ == "__main__":
+    
+    start = default_timer()
+
+    get_answer()
+
+    stop = default_timer()
+
+    print("Time taken -> ",stop - start)
