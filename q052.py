c00110b0124d5faa7952726f302f864ccaf2f2e2
Harry <59126651+hdowens@users.noreply.github.com>
2022-05-18 20:30:57 +0100

    Update q052.py

--- q052.py
+++ q052.py
@@ -4,11 +4,17 @@ from itertools import count
 
 def compare(list1, list2):
     return Counter(list1) == Counter(list2)
+"""
+defintely the ugliest solution to date. Runs in < 1s though so I feel no need to optimise it.
+yes, you could make sure it only iterates over numbers which will produce a 6x which still is
+the same length, i.e. only 100 -> 166, 1000-1666 etc. Maybe if this appears in a later 
+problem I will.
 
-#defintely the ugliest solution to date. Runs in < 1s though so I feel no need to optimise it.
-#yes, you could make sure it only iterates over numbers which will produce a 6x which still is
-#the same length, i.e. only 100 -> 166, 1000-1666 etc. Maybe if this appears in a later 
-#problem I will.
+Interestingly, after reading through other solutions, the number is itself an interesting property.
+In the deciaml representation of 1 / 7, we have 0.142857 142857 142857 ...
+and 7 x 0.142857 = 0.99999
+So the answer shows up in other places too.
+"""
 def compute():
     for x in count(1):
         x_list = [int(x) for x in str(x)]
